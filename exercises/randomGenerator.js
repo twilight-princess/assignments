17134f244117bd5dee400227471479ef81a8435b
Wife <midna@localhost.localdomain>
2018-06-11 12:53:40 -0600

    adding more exercises

---
+++ exercises/randomGenerator.js
@@ -0,0 +1,18 @@
+function passwordGen(numb) {
+    var password = "";
+    while (password.length + 1 < numb) {
+        let ascii = Math.floor(Math.random() * 92) + 34;
+        removeEmpty = function() {
+            if ((ascii == 95) || (ascii == 47)) {
+                let ascii = Math.floor(Math.random() * 92) + 34;
+                removeEmpty();
+            } else {
+                return ascii;
+            }
+        }
+        password += String.fromCharCode(ascii);
+        console.log(ascii)
+    }
+    console.log('Your randomly genrated password is: \"' + password + '\"!');
+}
+passwordGen(7);
